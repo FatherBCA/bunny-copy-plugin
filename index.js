829479ce86cf10709ade246f2cd78b994249445a
IceHub <daniardursunov7499@gmail.com>
2026-07-12 00:35:58 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+import { metro, common } from "@bunny";
+const { clipboard } = common;
+
+const ChatMessageText = metro.findByProps("ChatMessageText") || metro.findByName("ChatMessageText", false);
+
+let unpatch;
+
+export default {
+    onLoad: () => {
+        if (!ChatMessageText) return;
+
+        unpatch = metro.before("default", ChatMessageText, (args) => {
+            const props = args[0];
+            if (!props || !props.message) return;
+
+            const originalOnPress = props.onPress;
+
+            props.onPress = (event) => {
+                const textToCopy = props.message.content;
+
+                if (textToCopy) {
+                    clipboard.setString(textToCopy);
+                    if (common.toast) {
+                        common.toast.show("⚡ Copied!", 0);
+                    }
+                }
+
+                if (originalOnPress) {
+                    originalOnPress(event);
+                }
+            };
+        });
+    },
+    onUnload: () => {
+        if (unpatch) unpatch();
+    }
+};
